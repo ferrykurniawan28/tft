e45cda7d4816bcc3b513951fa28458fc4b96496a
ferrykurniawan28 <147892622+ferrykurniawan28@users.noreply.github.com>
2025-11-20 11:40:59 +0700

    init

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/safer/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/safer/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/safer/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
